69061d1ddc24e9e33dee7d990d0cd1d5ccf9c06e
Lawrence <winooxx@live.com>
2018-01-16 18:13:24 +0800

    Responsive canvas

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,10 +1,51 @@
-<template lang="pug">
-  canvas
+<template lang='pug'>
+  canvas(ref='canvas')
 </template>
 
 <script>
+
+const setCanvasSize = function () {
+  const canvas = this.$refs.canvas
+  if (this.windowSize.width >= 500) {
+    canvas.width = 320
+    canvas.height = 480
+    canvas.style.border = '1px solid black'
+  } else {
+    canvas.width = this.windowSize.width
+    canvas.height = this.windowSize.height
+    canvas.style.border = ''
+  }
+}
+
+const setWindowSize = function () {
+  this.windowSize = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+}
+
 export default {
-  name: 'Canvas'
+  name: 'Canvas',
+  data: function () {
+    return {
+      windowSize: {
+        width: 0,
+        height: 0
+      }
+    }
+  },
+  mounted () {
+    setWindowSize.call(this)
+    setCanvasSize.call(this)
+    this.$nextTick(() => {
+      window.addEventListener('resize', setWindowSize.bind(this))
+    })
+  },
+  watch: {
+    windowSize: function () {
+      setCanvasSize.call(this)
+    }
+  }
 }
 </script>
 
@@ -16,5 +57,5 @@ canvas
   top 0
   bottom 0
   left 0
-  right 0 
+  right 0
 </style>
